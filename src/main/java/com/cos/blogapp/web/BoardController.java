03a297ab287650bcef40f25614d82905b2ca4b53
kangm1nsu <su3592@naver.com>
2021-09-15 10:59:02 +0900

    상세보기 완료

--- src/main/java/com/cos/blogapp/web/BoardController.java
+++ src/main/java/com/cos/blogapp/web/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -31,6 +32,20 @@ public class BoardController {
 
 	private final BoardRepository boardRepository;
 	private final HttpSession session;
+	
+	//쿼리스트링, pathvar >>디비 where에 걸리는 친구들
+	//1.컨트롤러 선정, 2.Http Method 선정, 3. 받을데이터가 있는지!!(body,쿼리스트링,pathVar)
+	//
+	@GetMapping("/board/{id}")
+	public String detail(@PathVariable int id, Model model) {
+		//select * from board where id = :id
+		Board boardEntity = boardRepository.findById(id).get();
+		model.addAttribute("boardEntity", boardEntity);
+		return "board/detail";
+		
+	}
+	
+	
 
 	@PostMapping("/board")
 	public @ResponseBody String save(@Valid BoardSaveReqDto dto, BindingResult bindingResult) {
