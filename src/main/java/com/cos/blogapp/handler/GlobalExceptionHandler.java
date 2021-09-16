31ff0a7163997305a0f47303d1ee0f6eda1ee9f0
kangm1nsu <su3592@naver.com>
2021-09-16 11:43:46 +0900

    커스텀 익셉션 처리

--- src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.blogapp.handler.ex.MyNotFoundException;
 import com.cos.blogapp.util.Script;
 
 //@ControllerAdvice 1.익셉션 핸들링 2. @Controller의 역할까지 한다
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler(value = NoSuchElementException.class)
-	public @ResponseBody String error1(NoSuchElementException e) {
-		System.out.println("오류터졌어" + e.getMessage());
-		return Script.href("/","게시글을 찾을수없습니다.");
+	@ExceptionHandler(value = MyNotFoundException.class)
+	public @ResponseBody String error1(MyNotFoundException e) {
+		System.out.println("오류터졌어 : " + e.getMessage());
+		return Script.href("/", e.getMessage());
 	}
 }
