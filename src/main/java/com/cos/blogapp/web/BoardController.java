ee6d4d111c0f79972dccf06fac0ca9650ee6c656
kangm1nsu <su3592@naver.com>
2021-10-05 20:29:17 +0900

    서비스 완료

--- src/main/java/com/cos/blogapp/web/BoardController.java
+++ src/main/java/com/cos/blogapp/web/BoardController.java
@@ -6,9 +6,6 @@ import java.util.Map;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,14 +18,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cos.blogapp.domain.board.Board;
-import com.cos.blogapp.domain.board.BoardRepository;
 import com.cos.blogapp.domain.user.User;
 import com.cos.blogapp.handler.ex.MyAsyncNotFoundException;
-import com.cos.blogapp.handler.ex.MyNotFoundException;
+import com.cos.blogapp.service.BoardService;
 import com.cos.blogapp.util.Script;
 import com.cos.blogapp.web.dto.BoardSaveReqDto;
 import com.cos.blogapp.web.dto.CMRespDto;
+import com.cos.blogapp.web.dto.CommentSaveReqDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -36,109 +32,69 @@ import lombok.RequiredArgsConstructor;
 @Controller // 컴퍼넌트 스캔 (스프링) IoC
 public class BoardController {
 
-	private final BoardRepository boardRepository;
+	// DI
+	private final BoardService boardService;
 	private final HttpSession session;
 
-	@PutMapping("/board/{id}")
-	public @ResponseBody CMRespDto<String> update(@PathVariable int id, @Valid @RequestBody BoardSaveReqDto dto,
-			BindingResult bindingResult) {
-
-		// 인증
+	@PostMapping("/board/{boardId}/comment")
+	public String commentSave(@PathVariable int boardId, CommentSaveReqDto dto) {
 		User principal = (User) session.getAttribute("principal");
-		if (principal == null) {
-			throw new MyAsyncNotFoundException("로그인후 이용해주세요");
-		}
-		// 권한
+		boardService.댓글등록(boardId, dto, principal);
+		return "redirect:/board/" + boardId;
+	}
 
-		Board boardEntity = boardRepository.findById(id)
-				.orElseThrow(() -> new MyAsyncNotFoundException("해당글을 찾을수 없습니다"));
-		if (principal.getId() != boardEntity.getUser().getId()) {
-			throw new MyAsyncNotFoundException("해당글을 삭제할 권한이 없습니다");
-		}
-
-		// 유효성 검사
+	@PutMapping("/board/{id}")
+	public @ResponseBody CMRespDto<String> update(@PathVariable int id, @RequestBody @Valid BoardSaveReqDto dto,
+			BindingResult bindingResult) {
 
 		if (bindingResult.hasErrors()) {
 			Map<String, String> errorMap = new HashMap<>();
 			for (FieldError error : bindingResult.getFieldErrors()) {
 				errorMap.put(error.getField(), error.getDefaultMessage());
 			}
-			//return new CMRespDto<>(-1,"유효성 검사 실패",null);
 			throw new MyAsyncNotFoundException(errorMap.toString());
 		}
 
-		Board board = dto.toEntity(principal);
-		board.setId(id); // update의 핵심 같은 pk여야됨 나중에 서비스를 배우면 안쓰게됨
-
-		boardRepository.save(board);
-
+		User principal = (User) session.getAttribute("principal");
+		if (principal == null) {
+			throw new MyAsyncNotFoundException("인증이 되지 않았습니다");
+		}
+		
+		boardService.게시글수정(id, principal, dto);
 		return new CMRespDto<>(1, "업데이트 성공", null);
-
 	}
 
 	@GetMapping("/board/{id}/updateForm")
 	public String boardUpdateForm(@PathVariable int id, Model model) {
-		// 게시글 정보를 가지고 가야함.
-		Board boardEntity = boardRepository.findById(id)
-				.orElseThrow(() -> new MyNotFoundException(id + "번호의 게시글을 찾을 수 없습니다"));
-		model.addAttribute("boardEntity", boardEntity);
-
+		model.addAttribute("boardEntity", boardService.게시글수정페이지이동(id));
 		return "board/updateForm";
 	}
 
-	// API(AJAX) 요청
 	@DeleteMapping("/board/{id}")
 	public @ResponseBody CMRespDto<String> deleteById(@PathVariable int id) {
-
-		// 인증이 된 사람만 함수 접근 가능 !!(로그인 된 사람)
 		User principal = (User) session.getAttribute("principal");
 		if (principal == null) {
-			throw new MyAsyncNotFoundException("로그인후 이용해주세요");
+			throw new MyAsyncNotFoundException("인증이 되지 않았습니다.");
 		}
-
-		// 권한이 있는 사람만 함수 접근 가능 (principal.id=={id})
-
-		Board boardEntity = boardRepository.findById(id)
-				.orElseThrow(() -> new MyAsyncNotFoundException("해당글을 찾을수 없습니다"));
-		if (principal.getId() != boardEntity.getUser().getId()) {
-			throw new MyAsyncNotFoundException("해당글을 삭제할 권한이 없습니다");
-		}
-
-		try {
-			boardRepository.deleteById(id);
-		} catch (Exception e) {
-			throw new MyAsyncNotFoundException(id + "를 찾을 수 없어서 삭제할수없어요");
-		}
+		
+		boardService.게시글삭제(id, principal);
+	
 		return new CMRespDto<String>(1, "성공", null);
-
 	}
 
-	// 쿼리스트링, pathvar >>디비 where에 걸리는 친구들
-	// 1.컨트롤러 선정, 2.Http Method 선정, 3. 받을데이터가 있는지!!(body,쿼리스트링,pathVar)
-	//
 	@GetMapping("/board/{id}")
 	public String detail(@PathVariable int id, Model model) {
-		// select * from board where id = :id
-
-		// 1﻿.orElse(null) 값을 찾으면 Board가 리턴, 못찾으면 (괄호안 내용 리턴)
-//		Board boardEntity = boardRepository.findById(id)
-//			.orElse(new Board(100,"글없어요","글없어요",null));
-
-		// 2.orElse Throw
-		Board boardEntity = boardRepository.findById(id).orElseThrow(() -> new MyNotFoundException(id + " 못찾았어요"));
-
-		model.addAttribute("boardEntity", boardEntity);
+		model.addAttribute("boardEntity", boardService.게시글상세보기(id));
 		return "board/detail";
-
 	}
 
 	@PostMapping("/board")
 	public @ResponseBody String save(@Valid BoardSaveReqDto dto, BindingResult bindingResult) {
 
+		// 공통 로직 시작 -------------------------------------------
 		User principal = (User) session.getAttribute("principal");
 
-		// 인증체크
-		if (principal == null) { // 로그인 안됨
+		if (principal == null) {
 			return Script.href("/loginForm", "잘못된 접근입니다");
 		}
 
@@ -149,18 +105,12 @@ public class BoardController {
 			}
 			return Script.back(errorMap.toString());
 		}
-
-		System.out.println(dto.getTitle());
-		System.out.println(dto.getContent());
-		// 10시 15분까지 -> BoardSaveReqDto 생성
-		// Postman으로 테스트
-		// 콘솔에 출력
-
-//		User user = new User();
-//		user.setId(3);
-//		boardRepository.save(dto.toEntity(user));
-
-		boardRepository.save(dto.toEntity(principal));
+		// 공통 로직 끝 -------------------------------------------
+		
+		// 핵심 로직 시작 -------------------------------------------
+		boardService.게시글등록(dto, principal);
+		// 핵심 로직 끝 -------------------------------------------
+		
 		return Script.href("/", "글쓰기 성공");
 	}
 
@@ -169,14 +119,10 @@ public class BoardController {
 		return "board/saveForm";
 	}
 
+	// /board?page=2
 	@GetMapping("/board")
 	public String home(Model model, int page) {
-
-		PageRequest pageRequest = PageRequest.of(page, 3, Sort.by(Sort.Direction.DESC, "id"));
-
-		Page<Board> boardsEntity = boardRepository.findAll(pageRequest);
-		model.addAttribute("boardsEntity", boardsEntity);
-		// System.out.println(boardsEntity.get(0).getUser().getUsername());
+		model.addAttribute("boardsEntity", boardService.게시글목록보기(page));
 		return "board/list";
 	}
 }
--- src/main/webapp/WEB-INF/views/board/detail.jsp
+++ src/main/webapp/WEB-INF/views/board/detail.jsp
@@ -48,34 +48,34 @@
 	</div>
 	<hr />
 
-	<div class="card">
-		<form>
-			<div class="card-body">
-				<textarea id="reply-content" class="form-control" rows="1"></textarea>
-			</div>
-			<div class="card-footer">
-				<button type="button" id="btn-reply-save" class="btn btn-primary">등록</button>
-			</div>
-		</form>
-	</div>
-	<br />
 
-	<div class="card">
-		<div class="card-header">
-			<b>댓글 리스트</b>
-		</div>
-		<ul id="reply-box" class="list-group">
-			<li id="reply-1"
-				class="list-group-item d-flex justify-content-between">
-				<div>댓글입니다</div>
-				<div class="d-flex">
-					<div class="font-italic">작성자 : 홍길동 &nbsp;</div>
-					<button class="badge">삭제</button>
-				</div>
-			</li>
-		</ul>
-	</div>
-	<br />
+  <div class="card">
+      <form action="/board/${boardEntity.id}/comment" method="post">
+         <div class="card-body" >
+            <textarea name="content" id="reply-content" class="form-control" rows="1"></textarea>
+         </div>
+         <div class="card-footer">
+            <button type="submit" id="btn-reply-save" class="btn btn-primary">등록</button>
+         </div>
+      </form>
+   </div>
+   <br />
+   
+   <div class="card">
+      <div class="card-header"><b>댓글 리스트</b></div>
+      <ul id="reply-box" class="list-group">
+         <li id="reply-1" class="list-group-item d-flex justify-content-between">
+         <c:forEach>
+            <div>댓글입니다</div>
+            <div class="d-flex">
+               <div class="font-italic">작성자 : 홍길동 &nbsp;</div>
+               <button class="badge">삭제</button>
+            </div>
+         
+         </c:forEach>
+         </li>
+      </ul>
+   </div>
+   <br/>
 </div>
-
 <%@ include file="../layout/footer.jsp"%>
